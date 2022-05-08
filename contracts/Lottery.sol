8f34cb452b54a843a9304310775c78e96ad1d0f2
dogukanakkaya <doguakkaya27@hotmail.com>
2022-05-08 23:11:20 +0300

    feat: fetch eth_usd price feed from chainlink

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery is Ownable {
     enum State {
@@ -11,7 +12,13 @@ contract Lottery is Ownable {
         FINDING_WINNER
     }
 
-    State state;
+    State state = State.CLOSED;
+    AggregatorV3Interface ethUsdPriceFeed;
+    uint256 entranceFee = 1 * 10**18; // in usd with 18 decimals
+
+    constructor() {
+        ethUsdPriceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+    }
 
     function start() public {
         require(state == State.CLOSED, "Lottery is not closed yet!");
@@ -23,7 +30,13 @@ contract Lottery is Ownable {
         state = State.CLOSED;
     }
 
+    function getPrice() private view returns(uint256) {
+        (,int256 price,,,) = ethUsdPriceFeed.latestRoundData();
+
+        return uint256(price) * 10**10; // convert to 18 decimals
+    }
+
     function findWinner() public view onlyOwner returns(string memory) {
-        return "hi";
+        return "hi i am winner";
     }
 }
