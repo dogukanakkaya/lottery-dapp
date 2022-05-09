a2a26f8e249e93774abe1ec09e2e0b4d2286f1ee
dogukanakkaya <doguakkaya27@hotmail.com>
2022-05-09 23:06:21 +0300

    test: add enter function test

---
+++ test/lottery.js
@@ -0,0 +1,18 @@
+const Lottery = artifacts.require('Lottery');
+
+contract('Lottery', accounts => {
+    it('should return start(0) state', async () => {
+        const lottery = await Lottery.deployed();
+        await lottery.start();
+
+        const state = await lottery.getState();
+        assert.equal(state.toNumber(), 0);
+    });
+
+    it('should add new player', async () => {
+        const lottery = await Lottery.deployed();
+
+        const enter = await lottery.enter("dogukan", { from: accounts[0], value: web3.utils.toWei('0.001', 'ether') });
+        assert.isTrue(enter.receipt.status)
+    });
+});
