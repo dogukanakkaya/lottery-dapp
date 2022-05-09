2dc1efa6aef2778b558c548515d62d4977e500e9
dogukanakkaya <doguakkaya27@hotmail.com>
2022-05-09 23:36:52 +0300

    test: add closed test

--- test/lottery.js
+++ test/lottery.js
@@ -15,4 +15,12 @@ contract('Lottery', accounts => {
         const enter = await lottery.enter("dogukan", { from: accounts[0], value: web3.utils.toWei('0.001', 'ether') });
         assert.isTrue(enter.receipt.status)
     });
+
+    it('should return closed(1) state', async () => {
+        const lottery = await Lottery.deployed();
+        await lottery.close();
+
+        const state = await lottery.getState();
+        assert.equal(state.toNumber(), 1);
+    });
 });
