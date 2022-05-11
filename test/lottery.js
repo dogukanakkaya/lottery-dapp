d1c483738fad754730342e473581f5c92afb289a
dogukanakkaya <doguakkaya27@hotmail.com>
2022-05-11 22:06:21 +0300

    test: refactor tests and add todo for mocks

--- test/lottery.js
+++ test/lottery.js
@@ -1,8 +1,14 @@
 const Lottery = artifacts.require('Lottery');
 
 contract('Lottery', accounts => {
+    let lottery;
+
+    before(async () => {
+        lottery = await Lottery.deployed();
+        // lottery = await Lottery.at('0x7ea00526db7d121c2dafbfdd5f2e41fb5e1b5350');
+    })
+
     it('should return start(0) state', async () => {
-        const lottery = await Lottery.deployed();
         await lottery.start();
 
         const state = await lottery.getState();
@@ -10,16 +16,14 @@ contract('Lottery', accounts => {
     });
 
     if (config.network !== 'development') {
+        // todo: deploy mocks for this one
         it('should add new player', async () => {
-            const lottery = await Lottery.deployed();
-
             const enter = await lottery.enter("dogukan", { from: accounts[0], value: web3.utils.toWei('0.001', 'ether') });
             assert.isTrue(enter.receipt.status)
         });
     }
 
     it('should return closed(1) state', async () => {
-        const lottery = await Lottery.deployed();
         await lottery.close();
 
         const state = await lottery.getState();
