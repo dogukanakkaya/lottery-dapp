61dd7b8ee93b7c5edaef7eab41c7015bf96fc5a0
dogukanakkaya <doguakkaya27@hotmail.com>
2022-05-13 01:11:46 +0300

    refactor: no need to deploy vrf mock if non development

--- migrations/2_deploy_lottery.js
+++ migrations/2_deploy_lottery.js
@@ -10,18 +10,18 @@ module.exports = function (deployer) {
     vrfKeyHash = '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc';
     vrfSubscriptionId = 1;
     priceFeedAddress = '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e';
+
+    deployer.deploy(
+      VRFCoordinatorV2Mock,
+      10000000,
+      10000000
+    );
   } else {
     vrfCoordinatorAddress = '0x6168499c0cFfCaCD319c818142124B7A15E857ab';
     vrfKeyHash = '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc';
     vrfSubscriptionId = VRF_SUBSCRIPTION_ID;
     priceFeedAddress = '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e';
   }
-
-  deployer.deploy(
-    VRFCoordinatorV2Mock,
-    10000000,
-    10000000
-  );
 
   deployer.deploy(
     Lottery,
